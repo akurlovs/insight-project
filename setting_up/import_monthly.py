cca7b5ade01883698f3fd28e3a53a58a37b56dc4
akurlovs <34013941+akurlovs@users.noreply.github.com>
2020-01-27 23:21:53 -0800

    Add files via upload

--- setting_up/import_monthly.py
+++ setting_up/import_monthly.py
@@ -15,5 +15,5 @@ def listFD(url):
 for filey in listFD(url):
     if len(filey.split("/")[-1].split(".")[-1]) > 0:
         filey_out = filey.split("/")[-1]
-        print(f"wget -qO- {filey} | aws s3 cp - s3://{bucket}/{filey_out}")
+        print(f"wget -qO- {filey} | sed 's/ //g' - | aws s3 cp - s3://{bucket}/{filey_out}")
         subprocess.call(f"wget -qO- {filey} | sed 's/ //g' - | aws s3 cp - s3://{bucket}/{filey_out}", shell=True)
