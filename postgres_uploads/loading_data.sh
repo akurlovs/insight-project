467395ec84f639888f2c872dce415c50fafb628e
akurlovs <34013941+akurlovs@users.noreply.github.com>
2020-02-07 16:24:42 -0800

    Update loading_data.sh

--- postgres_uploads/loading_data.sh
+++ postgres_uploads/loading_data.sh
@@ -2,4 +2,4 @@
 spark-submit --master yarn --deploy-mode client --jars /home/hadoop/postgresql-42.2.9.jar spark_job_monthly_process_emr.py --driver-memory 15G
 
 # loading quarterly data
-spark-submit --master yarn --deploy-mode client --jars /home/hadoop/postgresql-42.2.9.jar spark_job_quarterly_process_emr2.py --driver-memory 15G
+spark-submit --master yarn --deploy-mode client --jars /home/hadoop/postgresql-42.2.9.jar spark_job_quarterly_process_emr3.py --driver-memory 15G
